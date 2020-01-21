967ec5936b2ecca66b0d47842daf63b85d59d963
German Poblete <mangerpv@gmail.com>
2020-01-21 17:40:40 -0300

    begin

---
+++ src/component/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => (
+
+  <div>Login
+    <form>
+      <input type="text" name="username"></input>
+      <input type="password" name="password"></input> 
+    </form>
+  </div>
+);
+
+export default Login;
