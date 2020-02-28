9a464e846267ee479cf2a9fe4e5f3e975c2e5279
German Poblete <mangerpv@gmail.com>
2020-02-28 16:11:16 -0300

    changes

--- src/component/home.js
+++ src/component/home.js
@@ -6,30 +6,52 @@ import { withRouter } from 'react-router-dom';
 class Character extends React.Component {
     Character = (props) => {
         return (
-            <div>{ props.character}</div>
+            <div>{ props.character.name}</div>
         );
     }
 };
+// class ListArray extends React.Component {
+//     Character = (props) => {
+//         const results = props.results;
+//         console.log('results', props);
+//         const listItems = results.map((character) =>
+//           <Character character={character} />
+//         );
+//         return (
+//           <div>
+//             {listItems}
+//           </div>
+//         );
+//     }
+// };
 
 class Home extends React.Component {
     constructor(props){
         super(props);
-        this.rickAndMorty = {};
+        this.state = {
+            // rickAndMortyData : {}
+        };
     }
 
-    ListArray = (props) => {
-        const results = props.results;
-        const listItems = results.map((character) =>
-          <Character character={character} />
-        );
-        return (
-          <div>
-            {listItems}
-          </div>
-        );
-      }
+    // ListArray = (props) => {
+    //     // const results = props.results;
+    //     console.log('results', props);
+    //     if (this.state.rickAndMortyData) {
+    //     const listItems = this.state.rickAndMortyDatafr.map((character) =>
+    //       <Character character={character} />
+    //     );
+    //     return (
+    //       <div>
+    //         {listItems}
+    //       </div>
+    //     );}
+    //   }
       
-      
+      Character = (props) => {
+            return (
+                <div>{ props.character.name}</div>
+            );
+        } 
 
     verifyRoute = () => {
         // console.log('TOKENNN:::',localStorage.getItem('token'));
@@ -40,7 +62,11 @@ class Home extends React.Component {
         axios.get('https://rickandmortyapi.com/api/character')
         .then(function (response) {
             // handle success
-            console.log('RICK:', response);
+            console.log('response::',response.data.results);
+            return response.data.results;
+        }).then(data => {
+            console.log('dataaa:',data);
+            this.setState({rickAndMortyData: data});
         })
         .catch(function (error) {
             // handle error
@@ -48,14 +74,18 @@ class Home extends React.Component {
         });
     };
 
-    render() {
+    componentDidMount(){
         this.verifyRoute();
+    }
+
+    render() {
+        // this.verifyRoute();
         return (
-            <div>
-                <p>Usted está logueado</p>
-                {/* <Out /> */}
-            </div>
-        );
+                this.state.rickAndMortyData.map((character) => {
+                    // <Character/>
+                })
+            
+        )
     }
 }
 
